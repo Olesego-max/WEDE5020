32454a7d7dcf149f5d5643a250f2ad82b5cf8937
Olesego-max <st10528325@rcconnect.edu.za>
2026-06-19 11:40:58 -0700

    Add files via upload

---
+++ js_assets/javascript.js
@@ -0,0 +1,7 @@
+
+
+light.option({
+    'resizeDuration': 200, //Image resizing animation take 0,2 seconds
+    'wraparound': true, //Last image links back to the first image
+    'fadeDuration': 300, //Fade effect takes 0.3 seconds
+})
